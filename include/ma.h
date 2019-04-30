be687e339aa7fa817824db94498f78311ae9c146
Manilator <manilator777@gmail.com>
2019-04-30 15:32:26 +0100

    Funções relacionadas com a Manutenção de Artigos feitas s/Documentação

--- include/ma.h
+++ include/ma.h
@@ -1,4 +1,42 @@
 #ifndef MANUTENCAO_H
 #define MANUTENCAO_H
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+//PARA REMOVER
+#include <stdio.h>
+
+struct Artigo
+{
+    int posicao;
+    float preco;
+};
+
+typedef struct Artigo Artigo;
+
+Artigo new_artigo(int posicao, float preco);
+
+Artigo clone_artigo(Artigo artigo);
+
+void store_artigo(Artigo artigo);
+
+int store_string(char *nome, int posicao_atual);
+
+int store(float preco, char *nome, int posicao_atual);
+
+int altera_nome(int codigo, char *nome, int posicao_atual);
+
+void altera_preco(int codigo, float preco);
+
+int ma_inserir(char *buf, int posicao_atual, int codigo_atual, int l);
+
+void ma_altera_nome(char *buf, int posicao_atual, int l);
+
+void ma_altera_preco(char *buf, int posicao_atual, int l);
+
+
 #endif /* MANUTENCAO_H*/
