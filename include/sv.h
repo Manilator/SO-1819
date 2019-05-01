b091bf895f00700f284d2bc82d7fcce2f5bc6a33
Manilator <manilator777@gmail.com>
2019-05-01 14:42:39 +0100

    Update sv.h

--- include/sv.h
+++ include/sv.h
@@ -1,4 +1,44 @@
 #ifndef SERVIDOR_H
 #define SERVIDOR_H
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+
+struct Stock
+{
+    int codigo;
+    int stock;
+};
+
+struct Venda
+{
+    int codigo;
+    int quantidade;
+    float montante;
+};
+
+typedef struct Stock Stock;
+
+typedef struct Venda Venda;
+
+Stock new_stock(int codigo, int stock);
+
+Stock clone_stock(Stock stock);
+
+void cria_stock(int codigo, int stock);
+
+void atualiza_stock(int codigo, int stock);
+
+void ler_stock(int codigo);
+
+Venda nova_venda(int codigo, int quantidade, float montante);
+
+Venda clone_venda(Venda old_venda);
+
+void cria_venda(int codigo, int quantidade, float montante);
+
 #endif /* SERVIDOR_H*/
