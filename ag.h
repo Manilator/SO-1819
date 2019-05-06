dd935ffd1b3310291fdfde80b2c747707a6d7de2
Gonçalo Almeida <48161282+Gonkalos@users.noreply.github.com>
2019-05-06 14:45:09 +0100

    Add files via upload

---
+++ ag.h
@@ -0,0 +1,34 @@
+
+#ifndef AGREGADOR_H
+#define AGREGADOR_H
+
+// Structs
+
+struct Agregador{
+    int codigo;
+    int vendas;
+    int quantidade;
+    float montante;
+};
+
+typedef struct Agregador Agregador;
+
+// API
+
+Agregador new_agregador (int codigo, int vendas, int quantidade, float montante);
+
+Agregador clone_agregador (Agregador ag);
+
+void store_agregador (Agregador ag);
+
+void cria_agregador (int codigo, int vendas, int quantidade, float montante);
+
+void atualiza_agregador (int codigo, int quantidade, float montante);
+
+Agregador ler_agregador (int codigo);
+
+void print_agregador (Agregador ag);
+
+/* void ler_agregador (int codigo); */
+
+#endif
